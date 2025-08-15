d81a4be19ac9ec731239e2ebda44f8245c6a8864
PaulyDuk <me@paulyd.co.uk>
2025-08-15 14:25:05 +0100

    Added top 10 scores logic to show

--- hackathon2/views.py
+++ hackathon2/views.py
@@ -8,4 +8,8 @@ from .models import game_scores  # or GameScore, use your actual model name
 def home(request):
     high_score_obj = game_scores.objects.order_by('-score').first()
     high_score = high_score_obj.score if high_score_obj else 0
-    return render(request, 'hackathon2/snake.html', {'high_score': high_score})
+    top_scores = game_scores.objects.order_by('-score')[:10]
+    return render(request, 'hackathon2/snake.html', {
+        'high_score': high_score,
+        'top_scores': top_scores
+    })

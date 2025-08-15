ddd933b5ee5485bd3cfbb5f0f4fe314e9d46c1cb
wen-l-liu <w.l.liu180@gmail.com>
2025-08-15 12:01:16 +0100

    Add Snake Game implementation with HTML, CSS, and JavaScript files

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     'SECRET_KEY',
-    'django-insecure-@x(kwsi^p^p79-&y7@#xmfy_-xr&vcki88nztfk0r%iqhb*uwd')
+    'django-insecure-@x(kwsi^p^p79-&y7@#xmfy_-xr&vcki88nztfk0rqhb*uwd')
